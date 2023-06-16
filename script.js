e229db73a8694365a090d4a488c8cf09aa3399b9
ajvw2 <129743473+ajvw2@users.noreply.github.com>
2023-06-17 00:06:37 +0200

    Clean up JS code, improve performance

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-function setGrid() {
-    let pixelPercentage = 100 / gridSize;
-    for (let i = 0; i < gridSize ** 2; i++) {
+function makeGrid() {
+    let pixelAmount = gridSize ** 2;
+    let widthPercentage = 100 / gridSize;
+    
+    for (let i = 0; i < pixelAmount; i++) {
         let newPixel = document.createElement('div');
         newPixel.classList.add('pixel');
+
+        let onLastColumn = (i % gridSize) === (gridSize - 1);
+        let onLastRow = i > (gridSize * (gridSize - 1) - 1);
+        if (onLastColumn) { newPixel.classList.add('last-column'); }
+        if (onLastRow) { newPixel.classList.add('last-row'); }
+
         newPixel.setAttribute('id', `pixel${i}`);
-        newPixel.style.cssText = `flex: 1 0 ${pixelPercentage}%`;
-        if (i % gridSize === gridSize - 1)
-        {
-            newPixel.style.borderRight = '0px';
-        }
-        if (i > gridSize * (gridSize - 1) - 1) {
-            newPixel.style.borderBottom = '0px';
-        }
+        newPixel.style.flex = `1 0 ${widthPercentage}%`;
         grid.appendChild(newPixel);
     }
 }
 
-function delay(ms) {
-    return new Promise((resolution) => {setTimeout(() => resolution('done!'), ms)});
-}
-
 async function clearGrid() {
-    // Setting overflow to hidden during animation lets the sketcher
-    // clip when it goes offscreen without scroll bars appearing
-    body.style.overflow = 'hidden';
-
-    let shakeNumber = Math.floor(Math.random() * 2) + 1;
-
+    // Select random shaking animation
+    // let shakeNumber = Math.floor(Math.random() * 2) + 1;
     // sketcher.classList.add(`shake${shakeNumber}`);
-    for (let i = 0; i < gridSize ** 2; i++) {
+    // Prevent appearance of scroll bars during animation
+    // body.style.overflow = 'hidden';
+    
+    let pixelAmount = gridSize ** 2;
+
+    for (let i = 0; i < pixelAmount; i++) {
         let selectedPixel = document.querySelector(`#pixel${i}`);
+
+        // Class '.erasure' increases transition time
         selectedPixel.classList.add('erasure');
+
+        // Remove (background) color from pixel
         selectedPixel.style.backgroundColor = 'transparent';
         
+        // Create sweeping animation while clearing
         if (i % gridSize === 0) {
-            selectedPixel.innerHTML = "&nbsp;";
-            await delay(750/gridSize);
-            selectedPixel.innerHTML = "";
+            // selectedPixel.innerHTML = "&nbsp;";
+            let delayTime = Math.max((750 / gridSize), 15);
+            await delay(delayTime);
+            // selectedPixel.innerHTML = "";
         }
     }
 
@@ -44,14 +48,11 @@ async function clearGrid() {
         pixel.classList.remove('erasure');
     })
 
-    
-
     // sketcher.addEventListener('animationiteration', () => {
     //     sketcher.classList.remove(`shake${shakeNumber}`);
     // });
-
-    await delay(1200);
-    body.style.overflow = 'visible';
+    //await delay(1200);
+    // body.style.overflow = 'visible';
 }
 
 function freeGrid() {
@@ -60,110 +61,96 @@ function freeGrid() {
     })
 }
 
+function updateGrid() {
+    freeGrid();
+    gridSize = gridSizeSlider.value;
+    makeGrid();
+    pixels = document.querySelectorAll('.pixel');
+    updateGridLines();
+    drawMouse();
+}
+
+function addGridLines() {
+    pixels.forEach((pixel) => {
+        pixel.classList.add('grid-lines-on');
+    });
+}
+
+function removeGridLines() {
+    pixels.forEach((pixel) => {
+        pixel.classList.remove('grid-lines-on');
+    });  
+}
+
+function updateGridLines() {
+    gridToggler.checked ? addGridLines() : removeGridLines();
+}
+
+function drawMouse() {
+    let mouseIsDown = false;
+    window.onmousedown = () => { mouseIsDown = true; }
+    window.onmouseup = () => { mouseIsDown = false; }
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            // Color on mouseover if mouse is down, else only 
+            // color on mousedown
+            if (mouseIsDown) {
+                pixel.style.backgroundColor = selectedColor;
+            } else {
+                pixel.addEventListener('mousedown', () => {
+                    pixel.style.backgroundColor = selectedColor;
+                });
+            }
+        });
+    });
+}
+
+function drawTouch() {
+    grid.addEventListener('touchmove', function(e) {
+        // Credit to: https://gist.github.com/VehpuS/6fd5dca2ea8cd0eb0471
+        let touch = e.touches[0];
+        let pixel = document.elementFromPoint(touch.clientX, touch.clientY);
+        pixel.style.backgroundColor = selectedColor;
+    });
+}
+
+function delay(ms) {
+    return new Promise((resolution) => {setTimeout(() => resolution('done!'), ms)});
+}
+
+// Main elements
 const body = document.querySelector('body');
 const sketcher = document.querySelector('.sketcher');
 const grid = document.querySelector('.pixel-grid');
 
-// Grid size slider
+// Control box 4 elements
 const gridSizeSlider = document.querySelector('#grid-size-slider');
+const gridToggler = document.querySelector('#grid-switch-checkbox');
+const clearButton = document.querySelector("#clear");
+
+
+let selectedColor = 'rgba(0, 0, 0, 0.700)';
+
+// Get initial grid size from slider and make the grid
 let gridSize = gridSizeSlider.value;
-
-setGrid();
+makeGrid();
+// Select pixels created by makeGrid()
 let pixels = document.querySelectorAll('.pixel');
-// Drawing with mouse held down
-let mouseIsDown = false;
-window.onmousedown = () => {
-    mouseIsDown = true;
-}
-window.onmouseup = () => {
-    mouseIsDown = false;
-}
+updateGridLines();
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-        if (mouseIsDown) {
-            pixel.style.backgroundColor = 'rgba(0, 0, 0, 0.700)';
-        } else {
-            pixel.addEventListener('mousedown', () => {
-                pixel.style.backgroundColor = 'rgba(0, 0, 0, 0.700)';
-            });
-        }
-    })
-});
+// Listen for drawing events
+drawMouse();
+drawTouch();
 
-
-
-gridSizeSlider.addEventListener('change', () => {
-    freeGrid();
-    gridSize = gridSizeSlider.value;
-    setGrid();
-    pixels = document.querySelectorAll('.pixel');
-    pixels.forEach((pixel) => {
-        pixel.addEventListener('mouseover', () => {
-            if (mouseIsDown) {
-                pixel.style.backgroundColor = 'rgba(0, 0, 0, 0.700)';
-            } else {
-                pixel.addEventListener('mousedown', () => {
-                    pixel.style.backgroundColor = 'rgba(0, 0, 0, 0.700)';
-                });
-            }
-        })
-    });
-});
-
-
-
-
-
-
-
-// Drawing on touchscreen
-grid.addEventListener('touchmove', function(e) {
-    // Credit to: https://gist.github.com/VehpuS/6fd5dca2ea8cd0eb0471
-    let touch = e.touches[0];
-    let pixel = document.elementFromPoint(touch.clientX, touch.clientY);
-    pixel.style.backgroundColor = 'rgba(0, 0, 0, 0.700)';
-});
-
+// Grid size slider
+gridSizeSlider.addEventListener('change', updateGrid);
 
 // Clear grid button
-clearButton = document.querySelector("#clear");
-clearButton.addEventListener('click', () => {
-    clearGrid();
-})
+clearButton.addEventListener('click', clearGrid);
 
 // Grid display switch
-const gridToggler = document.querySelector('#grid-switch-checkbox');
-gridToggler.addEventListener('change', () => {
-    if (gridToggler.checked) {
-        console.log('Show grid');
-        pixels.forEach((pixel) => {
-            
-            pixel.style.borderRadius = '2px';
-
-            let pixelNumber = pixel.id.match(/\d/g).join("");
-            if (pixelNumber % gridSize === gridSize - 1)
-            {
-                pixel.style.borderRight = '0px';
-            } else {
-                pixel.style.borderRight = '1px dashed rgba(128, 128, 128, 0.568)';
-            }
-            if (pixelNumber > gridSize * (gridSize - 1) - 1) {
-                pixel.style.borderBottom = '0px';
-            } else {
-                pixel.style.borderBottom = '1px dashed rgba(128, 128, 128, 0.568)';
-            }
-        })
-    }
-    else {
-        console.log('Don\'t show grid');
-        pixels.forEach((pixel) => {
-            pixel.style.border = '0';
-            pixel.style.borderRadius = '0';
-
-        })
-    }
-});
+gridToggler.addEventListener('change', updateGridLines);
 
 
 
